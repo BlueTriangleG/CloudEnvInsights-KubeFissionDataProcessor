a72c6988a1ac3096eea1b65959a6d6b6df6e5c93
GAOYUAN HAO <123238461+BlueTriangleG@users.noreply.github.com>
2024-05-21 13:19:28 +1000

    integrate elastic search download api

--- backend/apiInterface/functions/get-timeperioddata/get-timeperioddata.py
+++ backend/apiInterface/functions/get-timeperioddata/get-timeperioddata.py
@@ -1,7 +1,7 @@
 import logging
 import json
 from flask import Flask, request, Response
-from elasticsearch import Elasticsearch
+from elasticsearch8 import Elasticsearch
 from string import Template
 from datetime import datetime, timezone
 
@@ -11,10 +11,6 @@ ES_HOST = 'https://elasticsearch-master.elastic.svc.cluster.local:9200'
 ES_USER = 'elastic'
 ES_PASSWORD = 'elastic'
 
-# Define query template
-all_data_expr = Template('''{
-    "match_all": {}
-}''')
 
 time_period_expr_aircondition = Template('''{
     "range": {
@@ -37,23 +33,21 @@ time_period_expr_weathercondition = Template('''{
 }''')
 
 time_period_expr_mastodon_aus_social = Template('''{
-    "match_all": {}
+    "range": {
+        "created_at": {
+            "gte": "${start_date}",
+            "lte": "${end_date}"
+        }
+    }
 }''')
 
 def convert_date(date_string):
-    # 解析日期字符串为datetime对象
     date_obj = datetime.strptime(date_string, "%Y-%m-%d-%H")
-    
-    # 将datetime对象转换为ISO 8601格式的字符串，并添加时区信息（假设为UTC）
     iso_format_str = date_obj.replace(tzinfo=timezone.utc).isoformat()
-    
-    # 记录转换后的日期
     logging.info(f"Converted date: {iso_format_str}")
-    
-    # 确保返回值为字符串
     return str(iso_format_str)
 
-def generate_chunks(client, datatype, query, scroll_timeout="2m", page_size=1000):
+def generate_chunks(client, datatype, query, scroll_timeout="5m", page_size=1000):
     try:
         res = client.search(
             index=datatype,
@@ -78,7 +72,7 @@ def generate_chunks(client, datatype, query, scroll_timeout="2m", page_size=1000
         client.clear_scroll(scroll_id=scroll_id)
 
     except Exception as e:
-        logging.error(f"Second Error fetching data from Elasticsearch: {e}")
+        logging.error(f"Error fetching data from Elasticsearch: {e}")
         yield json.dumps({"error": str(e)}) + '\n'
     
 def main():
@@ -101,17 +95,16 @@ def main():
             basic_auth=(ES_USER, ES_PASSWORD)
         )
 
-        if end_date is None:
-            query = all_data_expr.substitute()
+        if datatype == 'aircondition':
+            query = time_period_expr_aircondition.substitute(start_date=start_date, end_date=end_date)
+        elif datatype == 'weathercondition':
+            query = time_period_expr_weathercondition.substitute(start_date=start_date, end_date=end_date)
+        elif datatype == 'mastodon-aus-social':
+            start_date = convert_date(start_date)
+            end_date = convert_date(end_date)
+            query = time_period_expr_mastodon_aus_social.substitute(start_date=start_date, end_date=end_date)
         else:
-            if datatype == 'aircondition':
-                query = time_period_expr_aircondition.substitute(start_date=start_date, end_date=end_date)
-            elif datatype == 'weathercondition':
-                query = time_period_expr_weathercondition.substitute(start_date=start_date, end_date=end_date)
-            elif datatype == 'mastodon-aus-social':
-                start_date = convert_date(start_date)
-                end_date = convert_date(end_date)
-                query = all_data_expr.substitute()
+            return json.dumps({"error": "Invalid datatype"}), 400
 
         # execute query
         query_body = {
@@ -121,5 +114,5 @@ def main():
         return Response(generate_chunks(client, datatype, query_body), content_type='application/json')
 
     except Exception as e:
-        logging.error(f"First Error fetching data from Elasticsearch: {e}")
+        logging.error(f"Error fetching data from Elasticsearch: {e}")
         return json.dumps({"error": str(e)}), 500
